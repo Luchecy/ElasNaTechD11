cdaefe8eb8f14c2a8aa8f96b9d9bfeb27ac86130
luchecy <mar_luchecy@hotmail.com>
2023-08-03 21:25:08 -0300

    Finalizado

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+var numero = parseInt(prompt('Digite um número'));
+
+if(numero >0 && numero <100 && numero % 2 == 0){
+    alert("O número atende aos critérios");
+} else{
+    alert("O número não atende aos critérios");
+}
